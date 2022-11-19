8c328a4c5461625164489e254b82da0fd4143ffa
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:22:35 +0800

    Update wattpad.py

--- src/wattpad.py
+++ src/wattpad.py
@@ -5,7 +5,7 @@ class WattPad:
 	def __init__(
 			self,
 			locale: str = "en_US",
-			language_id: int = 1):
+			language_id: int = 1) -> None:
 		self.first_api = "https://www.wattpad.com"
 		self.second_api = "https://api.wattpad.com"
 		self.locale = locale
@@ -22,7 +22,7 @@ class WattPad:
 			self,
 			username: str,
 			password: str,
-			fields: str = "token,ga,user(username,description,avatar,name,email,genderCode,language,birthdate,verified,isPrivate,ambassador,is_staff,follower,following,backgroundUrl,votesReceived,numFollowing,numFollowers,createDate,followerRequest,website,facebook,twitter,followingRequest,numStoriesPublished,numLists,location,externalId,programs,showSocialNetwork,verified_email,has_accepted_latest_tos,email_reverification_status,language,inbox(unread),has_password,connectedServices)"):
+			fields: str = "token,ga,user(username,description,avatar,name,email,genderCode,language,birthdate,verified,isPrivate,ambassador,is_staff,follower,following,backgroundUrl,votesReceived,numFollowing,numFollowers,createDate,followerRequest,website,facebook,twitter,followingRequest,numStoriesPublished,numLists,location,externalId,programs,showSocialNetwork,verified_email,has_accepted_latest_tos,email_reverification_status,language,inbox(unread),has_password,connectedServices)") -> dict:
 		data = {
 			"type": "wattpad",
 			"username": username,
@@ -46,7 +46,7 @@ class WattPad:
 			password: str,
 			email: str,
 			has_accepted_latest_tos: bool = True,
-			fields: str = "token,ga,user(username,description,avatar,name,email,genderCode,language,birthdate,verified,isPrivate,ambassador,is_staff,follower,following,backgroundUrl,votesReceived,numFollowing,numFollowers,createDate,followerRequest,website,facebook,twitter,followingRequest,numStoriesPublished,numLists,location,externalId,programs,showSocialNetwork,verified_email,has_accepted_latest_tos,email_reverification_status,language,inbox(unread),has_password,connectedServices)"):
+			fields: str = "token,ga,user(username,description,avatar,name,email,genderCode,language,birthdate,verified,isPrivate,ambassador,is_staff,follower,following,backgroundUrl,votesReceived,numFollowing,numFollowers,createDate,followerRequest,website,facebook,twitter,followingRequest,numStoriesPublished,numLists,location,externalId,programs,showSocialNetwork,verified_email,has_accepted_latest_tos,email_reverification_status,language,inbox(unread),has_password,connectedServices)") -> dict:
 		data = {
 			"type": "wattpad",
 			"username": username,
@@ -63,22 +63,22 @@ class WattPad:
 			headers=self.headers).json()
 
 
-	def validate_email(self, email: str):
+	def validate_email(self, email: str) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/validate?email={email}",
 			headers=self.headers).json()
 
-	def validate_username(self, username: str):
+	def validate_username(self, username: str) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/validate?username={username}",
 			headers=self.headers).json()
 
-	def get_password_strength_policies(self):
+	def get_password_strength_policies(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/5/password-strength/policies",
 			headers=self.headers).json()
 
-	def check_password_strength(self, username: str, password: str):
+	def check_password_strength(self, username: str, password: str) -> dict:
 		data = {
 			"password": password,
 			"requester": {
@@ -90,12 +90,12 @@ class WattPad:
 			data=data,
 			headers=self.headers).json()
 
-	def get_language_ids(self):
+	def get_language_ids(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/apiv2/getlang",
 			headers=self.headers).json()
 
-	def get_categories(self):
+	def get_categories(self) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/categories?language={self.language_id}",
 			headers=self.headers).json()
@@ -104,7 +104,7 @@ class WattPad:
 			self,
 			username: str,
 			fields: str = "nextUrl,total,stories(id,title,name,cover,deleted,user,readingPosition,modifyDate)",
-			limit: int = 40):
+			limit: int = 40) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{username}/archive?fields={fields}&limit={limit}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -113,12 +113,12 @@ class WattPad:
 			self,
 			username: str,
 			fields: str = "lists(user,id,action,name,numStories,featured,promoted,description,cover),nextUrl",
-			limit: int = 10):
+			limit: int = 10) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{username}/lists?fields={fields}&limit={limit}&wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def get_user_wallet(self, username: str):
+	def get_user_wallet(self, username: str) -> dict:
 		return requests.get(
 			f"{self.second_api}/v5/users/{username}/wallet?wp_token={self.token}",
 			headers=self.headers).json()
@@ -127,7 +127,7 @@ class WattPad:
 			self,
 			username: str,
 			fields: str = "last_sync_timestamp,nextUrl,total,stories(id,title,length,createDate,modifyDate,promoted,user,description,cover,completed,isPaywalled,paidModel,categories,tags,numParts,readingPosition,deleted,dateAdded,lastPublishedPart(createDate),story_text_url(text),parts(id,title,modifyDate,length,deleted,text_url(text)))",
-			limit: int = 40):
+			limit: int = 40) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{username}/library?fields={fields}&limit={limit}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -136,12 +136,12 @@ class WattPad:
 			self,
 			username: str,
 			fields: str = "stories(id,title,length,createDate,modifyDate,voteCount,readCount,commentCount,language,user,description,cover,url,completed,isPaywalled,categories,tags,numParts,readingPosition,deleted,story_text_url(text),copyright,rating,mature,ratingLocked,tagRankings,hasBannedCover,parts(id,title,voteCount,commentCount,videoId,readCount,photoUrl,modifyDate,length,voted,deleted,text_url(text),dedication,url,wordCount,draft,hash,hasBannedImages),isAdExempt),nextUrl",
-			drafts: int = 1):
+			drafts: int = 1) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{username}/stories?fields={fields}&drafts={drafts}&wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def get_user_subscription_prompts(self, username: str):
+	def get_user_subscription_prompts(self, username: str) -> dict:
 		return requests.get(
 			f"{self.second_api}/v5/users/{username}/subscriptions/prompts?wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -149,22 +149,22 @@ class WattPad:
 	def get_user_published_stories(
 			self,
 			username: str,
-			fields: str = "stories(id),nextUrl"):
+			fields: str = "stories(id),nextUrl") -> dict:
 		return requests.get(
 			f"{self.second_api}/v4/users{username}/stories/published?fields={fields}&wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def get_browse_topics(self, type: str = "onboarding"):
+	def get_browse_topics(self, type: str = "onboarding") -> dict:
 		return requests.get(
 			f"{self.second_api}/v5/browse/topics?language={self.language_id}&type={type}&wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def get_products_list(self):
+	def get_products_list(self) -> dict:
 		return requests.get(
 			f"{self.second_api}/v5/subscriptions/products?wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def get_home_page(self):
+	def get_home_page(self) -> dict:
 		return requests.get(
 			f"{self.second_api}/v5/home?wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -174,7 +174,7 @@ class WattPad:
 			story_id: int,
 			drafts: int = 0,
 			include_deleted: int = 1,
-			fields: str = "id,title,length,createDate,modifyDate,voteCount,readCount,commentCount,url,promoted,sponsor,language,user,description,cover,highlight_colour,completed,isPaywalled,paidModel,categories,numParts,readingPosition,deleted,dateAdded,lastPublishedPart(createDate),tags,copyright,rating,story_text_url(text),,parts(id,title,voteCount,commentCount,videoId,readCount,photoUrl,modifyDate,length,voted,deleted,text_url(text),dedication,url,wordCount),isAdExempt,tagRankings"):
+			fields: str = "id,title,length,createDate,modifyDate,voteCount,readCount,commentCount,url,promoted,sponsor,language,user,description,cover,highlight_colour,completed,isPaywalled,paidModel,categories,numParts,readingPosition,deleted,dateAdded,lastPublishedPart(createDate),tags,copyright,rating,story_text_url(text),,parts(id,title,voteCount,commentCount,videoId,readCount,photoUrl,modifyDate,length,voted,deleted,text_url(text),dedication,url,wordCount),isAdExempt,tagRankings") -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/stories/{story_id}?drafts={drafts}&include_deleted={include_deleted}&fields={fields}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -183,7 +183,7 @@ class WattPad:
 			self,
 			story_id: int,
 			fields: str = "id,title,readCount,cover,isPaywalled",
-			limit: int = 10):
+			limit: int = 10) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/stories/{story_id}?fields={fields}&limit={limit}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -191,7 +191,7 @@ class WattPad:
 	def get_user_info(
 			self,
 			username: str,
-			fields: str = "username,description,avatar,name,email,genderCode,language,birthdate,verified,isPrivate,ambassador,is_staff,follower,following,backgroundUrl,votesReceived,numFollowing,numFollowers,createDate,followerRequest,website,facebook,twitter,followingRequest,numStoriesPublished,numLists,location,externalId,programs,showSocialNetwork,verified_email,has_accepted_latest_tos,email_reverification_status,highlight_colour,safety(isMuted,isBlocked)"):
+			fields: str = "username,description,avatar,name,email,genderCode,language,birthdate,verified,isPrivate,ambassador,is_staff,follower,following,backgroundUrl,votesReceived,numFollowing,numFollowers,createDate,followerRequest,website,facebook,twitter,followingRequest,numStoriesPublished,numLists,location,externalId,programs,showSocialNetwork,verified_email,has_accepted_latest_tos,email_reverification_status,highlight_colour,safety(isMuted,isBlocked)") -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{username}?fields={fields}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -199,7 +199,7 @@ class WattPad:
 	def get_user_followers(
 			self,
 			username: str,
-			fields: str = "users(username,avatar,location,numFollowers,following,follower,followingRequest,followerRequest),nextUrl"):
+			fields: str = "users(username,avatar,location,numFollowers,following,follower,followingRequest,followerRequest),nextUrl") -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{username}/followers?fields={fields}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -209,12 +209,12 @@ class WattPad:
 			username: str,
 			fields: str = "users(username,avatar,location,numFollowers,following,follower,followingRequest,followerRequest),nextUrl",
 			limit: int = 10,
-			offset: int = 10):
+			offset: int = 10) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{username}/following?fields={fields}&wp_token={self.wp_token}&limit={limit}&offset={offset}",
 			headers=self.headers).json()
 
-	def ignore_user(self, username: str):
+	def ignore_user(self, username: str) -> dict:
 		data = {
 			"id": username,
 			"action": "ignore_user"
@@ -224,7 +224,7 @@ class WattPad:
 			data=data,
 			headers=self.headers).json()
 
-	def unignore_user(self, username: str):
+	def unignore_user(self, username: str) -> dict:
 		data = {
 			"id": username,
 			"action": "unignore_user"
@@ -234,7 +234,7 @@ class WattPad:
 			data=data,
 			headers=self.headers).json()
 
-	def resend_email_verification(self):
+	def resend_email_verification(self) -> dict:
 		data = {
 			"activation_email": True
 		}
@@ -247,7 +247,7 @@ class WattPad:
 			self,
 			username: str,
 			fields: str = "messages(id,body,createDate,from,numReplies,isOffensive,isReply,latestReplies),total,nextUrl",
-			limit: int = 30):
+			limit: int = 30) -> dict:
 		return requests.get(
 			f"{self.second_api}/v4/users/{username}/messages?fields={fields}&return=data&limit={limit}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -256,7 +256,7 @@ class WattPad:
 			self,
 			email: str,
 			password: str,
-			authenticate: int = 1):
+			authenticate: int = 1) -> dict:
 		data = {
 			"id": self.user_id,
 			"email": email,
@@ -273,7 +273,7 @@ class WattPad:
 			self,
 			username: str,
 			message: str,
-			broadcast: int = 0):
+			broadcast: int = 0) -> dict:
 		data = {
 			"name": username,
 			"body": message,
@@ -284,7 +284,7 @@ class WattPad:
 			data=data,
 			headers=self.headers).json()
 
-	def delete_comment(self, username: str, message_id: int):
+	def delete_comment(self, username: str, message_id: int) -> dict:
 		return requests.delete(
 			f"{self.second_api}/v4/users/{username}/messages/{message_id}?wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -292,17 +292,17 @@ class WattPad:
 	def get_comment_replies(
 			self,
 			message_id: int,
-			fields: str = "replies(id,body,createDate,from),nextUrl"):
+			fields: str = "replies(id,body,createDate,from),nextUrl") -> dict:
 		return requests.get(
 			f"{self.second_api}/v4/messages/{message_id}?fields={fields}&wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def follow_user(self, username: str):
+	def follow_user(self, username: str) -> dict:
 		return requests.post(
 			f"{self.first_api}/api/v3/users/{self.username}/following?users={username}&wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def unfollow_user(self, username: str):
+	def unfollow_user(self, username: str) -> dict:
 		return requests.delete(
 			f"{self.first_api}/api/v3/users/{self.username}/following?users={username}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -312,7 +312,7 @@ class WattPad:
 			query: str,
 			fields: str = "username,avatar,following,name,verified,ambassador,is_staff,programs",
 			limit: int = 20,
-			offset: int = 20):
+			offset: int = 20) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users?fields={fields}&limit={limit}&offset={offset}&query={query}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -324,7 +324,7 @@ class WattPad:
 			free: int = 1,
 			paid: int = 1,
 			limit: int = 30,
-			fields: str = "stories(id,title,voteCount,readCount,numParts,tags,description,user,mature,completed,rating,cover,promoted,isPaywalled,lastPublishedPart,sponsor(name,avatar),tracking(clickUrl,impressionUrl,thirdParty(impressionUrls,clickUrls)),contest(endDate,ctaLabel)),tags,nextUrl,total"):
+			fields: str = "stories(id,title,voteCount,readCount,numParts,tags,description,user,mature,completed,rating,cover,promoted,isPaywalled,lastPublishedPart,sponsor(name,avatar),tracking(clickUrl,impressionUrl,thirdParty(impressionUrls,clickUrls)),contest(endDate,ctaLabel)),tags,nextUrl,total") -> dict:
 		return requests.get(
 			f"{self.second_api}/v4/search/stories?query={query}&mature={mature}&free={free}&paid={paid}&limit={limit}&fields={fields}&language={self.language_id}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -333,12 +333,12 @@ class WattPad:
 			self,
 			query: str,
 			limit: int = 10,
-			offset: int = 10):
+			offset: int = 10) -> dict:
 		return requests.get(
 			f"{self.first_api}/v4/lists?query={query}&wp_token={self.wp_token}&limit={limit}&offset={offset}",
 			headers=self.headers).json()
 
-	def send_message(self, username: str, message: str):
+	def send_message(self, username: str, message: str) -> dict:
 		data = {
 			"sender": self.username,
 			"recipient": username,
@@ -349,12 +349,12 @@ class WattPad:
 			data=data,
 			headers=self.headers).json()
 
-	def delete_chat(self, username: str):
+	def delete_chat(self, username: str) -> dict:
 		return requests.delete(
 			f"{self.first_api}/api/v3/users/{self.username}/inbox/{username}?wp_token={self.wp_token}",
 			headers=self.headers).json()
 
-	def get_started_chats(self, limit: int = 20):
+	def get_started_chats(self, limit: int = 20) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{self.username}/inbox?limit={limit}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -363,7 +363,7 @@ class WattPad:
 			self,
 			fields: str = "feed,nextUrl",
 			limit: int = 10,
-			direction: int = 0):
+			direction: int = 0) -> dict:
 		return requests.get(
 			f"{self.first_api}/api/v3/users/{self.username}/notifications?return=data&fields={fields}&limit={limit}&direction={direction}&wp_token={self.wp_token}",
 			headers=self.headers).json()
@@ -372,7 +372,7 @@ class WattPad:
 			self,
 			username: str,
 			password: str,
-			authenticate: int = 1):
+			authenticate: int = 1) -> dict:
 		data = {
 			"id": self.user_id,
 			"username": username,
@@ -384,15 +384,19 @@ class WattPad:
 			data=data,
 			headers=self.headers).json()
 
-	def update_fullname(self, fullname: str):
-		data = {"name": fullname}
+	def update_fullname(self, fullname: str) -> dict:
+		data = {
+			"name": fullname
+		}
 		return requests.put(
 			f"{self.first_api}/api/v3/users/{self.username}?wp_token={self.wp_token}",
 			data=data,
 			headers=self.headers).json()
 
-	def update_website(self, url: str):
-		data = {"website": url}
+	def update_website(self, url: str) -> dict:
+		data = {
+			"website": url
+		}
 		return requests.put(
 			f"{self.first_api}/api/v3/users/{self.username}?wp_token={self.wp_token}",
 			data=data,
@@ -402,7 +406,7 @@ class WattPad:
 			self,
 			old_password: str,
 			new_password: str,
-			has_password: int = 1):
+			has_password: int = 1) -> dict:
 		data = {
 			"id": self.user_id,
 			"new_password": new_password,
